84aab8a3e54e9913bbfd8ca82b3ec3e917954161
Johnny Wu <KarasAya@gmail.com>
2017-02-27 11:11:49 +0800

    simple refine

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 'use strict';
 
 const platform = require('electron-platform');
+const pkgJson = require('./package.json');
 
 let dockable;
+let name = `__electron_dockable__`;
+let msg = `Failed to require ${pkgJson.name}@${pkgJson.version}:
+  A different version of ${pkgJson.name} already running in the process, we will redirect to it.
+  Please make sure your dependencies use the same version of ${pkgJson.name}.`;
 
 if ( platform.isMainProcess ) {
-  dockable = require('./lib/main');
+  if (global[name]) {
+    console.warn(msg);
+    dockable = global[name];
+  } else {
+    dockable = global[name] = require('./lib/main');
+  }
 } else {
-  dockable = require('./lib/renderer/index');
+  if (window[name]) {
+    console.warn(msg);
+    dockable = window[name];
+  } else {
+    dockable = window[name] = require('./lib/renderer/index');
+  }
 }
 
 // ==========================
